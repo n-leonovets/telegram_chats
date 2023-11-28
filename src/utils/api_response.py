9c9532bf558b46b8637dc4fb246d1e78c6b67dc9
Nikolay Leonovets <work.nleonovec@gmail.com>
2023-11-28 18:34:20 +0300

    update api_response position

--- src/utils/api_response.py
+++ src/utils/api_response.py
@@ -6,6 +6,25 @@ from pydantic import BaseModel
 T = TypeVar("T")
 
 
+class ApiResponse(BaseModel, Generic[T]):
+    """
+    Стардартизированный ответ API.
+
+        Parameters:
+            status_code (``int``):
+                Статус ответ сервера.
+
+            data (Any | None) = None:
+                Выходные данные.
+
+            detail (str | None) = None:
+                Доп. информация (если есть).
+    """
+    status_code: int
+    data: Optional[T] = None
+    detail: Optional[str] = None
+
+
 class ApiErrorDetail(BaseModel):
     """
     Стардартизированный ответ ошибок API.
@@ -23,22 +42,3 @@ class ApiErrorDetail(BaseModel):
     module: str
     name: str
     message: str
-
-
-class ApiResponse(BaseModel, Generic[T]):
-    """
-    Стардартизированный ответ API.
-
-        Parameters:
-            status_code (``int``):
-                Статус ответ сервера.
-
-            data (Any | None) = None:
-                Выходные данные.
-
-            detail (str | None) = None:
-                Детали ошибок.
-    """
-    status_code: int
-    data: Optional[T] = None
-    detail: Optional[str] = None
