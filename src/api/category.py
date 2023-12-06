d2f5266713907641298f740144bbe1587b529b13
Nikolay Leonovets <work.nleonovec@gmail.com>
2023-12-06 13:35:52 +0300

    add category api

--- src/api/category.py
+++ src/api/category.py
@@ -5,6 +5,12 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.api.auth import required_auth
 from src.api.dependencies import UOWDep
+from src.schemas.category import Category, CategoryResponse
+from src.schemas.user import UserPublic
+from src.services.category import CategoryService
+from src.services.filters.base import LimitFilter
+from src.services.filters.category import CategoryFilter
+from src.utils.exception_detail import get_exception_detail
 
 router = APIRouter(
     prefix="/categories",
@@ -13,3 +19,85 @@ router = APIRouter(
 _logger = logging.getLogger(__name__)
 
 
+@router.post("/chat/")
+async def add_chat(
+    uow: UOWDep,
+    category: Category,
+    user_auth: UserPublic = Depends(required_auth)
+) -> CategoryResponse:
+    try:
+        return await CategoryService().add_category(uow=uow, category=category)
+    except Exception as e:
+        _logger.error("Exception error", exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=get_exception_detail(e)
+        )
+
+
+@router.post("/")
+async def add_chats(
+    uow: UOWDep,
+    categoris: list[Category],
+    user_auth: UserPublic = Depends(required_auth)
+) -> list[CategoryResponse]:
+    try:
+        return await CategoryService().add_categoris(uow=uow, categoris=categoris)
+    except Exception as e:
+        _logger.error("Exception error", exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=get_exception_detail(e)
+        )
+
+
+@router.get("/")
+async def get_chats(
+    uow: UOWDep,
+    filters: Annotated[CategoryFilter, Depends()],
+    limits: Annotated[LimitFilter, Depends()],
+    user_auth: UserPublic = Depends(required_auth)
+) -> list[CategoryResponse]:
+    try:
+        return await CategoryService().get_categories(uow=uow, filters=filters, limits=limits)
+    except Exception as e:
+        _logger.error("Exception error", exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=get_exception_detail(e)
+        )
+
+
+@router.patch("/{category_id}")
+async def update_chat(
+    uow: UOWDep,
+    category_id: int,
+    category: Category,
+    user_auth: UserPublic = Depends(required_auth)
+) -> CategoryResponse:
+    try:
+        return await CategoryService().update_category(
+            uow=uow, category=category, filters=CategoryFilter(id=category_id)
+        )
+    except Exception as e:
+        _logger.error("Exception error", exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=get_exception_detail(e)
+        )
+
+
+@router.delete("/{category_id}")
+async def delete_chat(
+    uow: UOWDep,
+    category_id: int,
+    user_auth: UserPublic = Depends(required_auth)
+) -> CategoryResponse:
+    try:
+        return await CategoryService().delete_category(uow=uow, filters=CategoryFilter(id=category_id))
+    except Exception as e:
+        _logger.error("Exception error", exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=get_exception_detail(e)
+        )
--- src/services/category.py
+++ src/services/category.py
@@ -9,30 +9,39 @@ from src.utils.unitofwork import AbstractUnitOfWork
 
 class CategoryService:
     @staticmethod
-    async def add_category(uow: AbstractUnitOfWork, category: Category):
+    async def add_category(uow: AbstractUnitOfWork, category: Category) -> CategoryResponse:
         async with uow:
             result: CategoryModel = await uow.category.create_one(values=category.model_dump())
             await uow.commit()
             return CategoryResponse(**result.__dict__)
 
     @staticmethod
+    async def add_categoris(uow: AbstractUnitOfWork, categoris: list[Category]) -> list[CategoryResponse]:
+        async with uow:
+            result: list[CategoryModel] = await uow.category.create_all(
+                values=[category.model_dump() for category in categoris]
+            )
+            await uow.commit()
+            return [CategoryResponse(**category.__dict__) for category in result]
+
+    @staticmethod
     async def get_categories(
         uow: AbstractUnitOfWork,
         filters: Optional[CategoryFilter] = None,
         limits: Optional[LimitFilter] = None
-    ):
+    ) -> list[CategoryResponse]:
         async with uow:
             result: list[CategoryModel] = await uow.category.read_all(filters=filters, limits=limits)
             return [CategoryResponse(**category.__dict__) for category in result]
 
     @staticmethod
-    async def get_category(uow: AbstractUnitOfWork, filters: CategoryFilter):
+    async def get_category(uow: AbstractUnitOfWork, filters: CategoryFilter) -> CategoryResponse:
         async with uow:
             result: CategoryModel = await uow.category.read_one(filters=filters)
             return CategoryResponse(**result.__dict__)
 
     @staticmethod
-    async def update_category(uow: AbstractUnitOfWork, category: Category, filters: CategoryFilter):
+    async def update_category(uow: AbstractUnitOfWork, category: Category, filters: CategoryFilter) -> CategoryResponse:
         async with uow:
             result: CategoryModel = await uow.category.update_one(
                 values=category.model_dump(exclude_none=True),
@@ -42,7 +51,7 @@ class CategoryService:
             return CategoryResponse(**result.__dict__)
 
     @staticmethod
-    async def delete_category(uow: AbstractUnitOfWork, filters: CategoryFilter):
+    async def delete_category(uow: AbstractUnitOfWork, filters: CategoryFilter) -> CategoryResponse:
         async with uow:
             result: CategoryModel = await uow.category.delete_one(filters=filters)
             await uow.commit()
